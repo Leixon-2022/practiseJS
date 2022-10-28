e2908e96068230525f4f6cacd775b64ffd97a9f2
Thomas Månsson <ThomasCreate@thomascreate.mynet>
2022-10-28 09:43:36 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+/*
+* Här är min javascript!
+*/
+
+let thomasDenBäste;
+
+thomasDenBäste = "Jag är bäst!!!";
+
+let sant = true;
+/*mitt svar: c*/
+console.log(thomasDenBäste + sant);
+
+let name =  prompt("Skriv ditt namn.");
+
+alert("Hejsan " + name + " " + thomasDenBäste + sant);
+
+
+
